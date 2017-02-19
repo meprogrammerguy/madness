a00d8595f1b3e03084d32de76daf19b0dabdae7e
meprogrammerguy <b65535@gmail.com>
2017-02-19 12:48:44 -0800

    testing

--- scrape.ps1
+++ scrape.ps1
@@ -10,6 +10,7 @@ function GetElapsedTime([datetime]$starttime)
   $retStr
 }
 $script:startTime = Get-Date
+$wc = New-Object System.Net.WebClient
 write-host "scrape Script Started at $script:startTime" -foreground "green"
 <#
     Opens the settings file
@@ -26,154 +27,22 @@ New-Item -ItemType Directory -Force -Path $WorkDirectory | Out-Null
 <#
     kenpom stats page
 #>
-$wc = New-Object System.Net.WebClient
-$WebPage1 = $ConfigFile.Settings.scrape.kenpom.WebPage
-$WebPage1 
-$content1 = $wc.DownloadString($WebPage1)
-$FilePath1 = $WorkDirectory + "\kenpom.html"
-$content1 | Out-File -FilePath $FilePath1
+$WebPage = $ConfigFile.Settings.scrape.kenpom.WebPage
+$TableNumber = $ConfigFile.Settings.scrape.kenpom.TableNumber
+$WebPage 
+$request = Invoke-WebRequest $WebPage
+$KenpomPath = $WorkDirectory + "\kenpom.txt"
+Get-PSBreakpoint | Remove-PSBreakpoint
+Set-PsBreakPoint extract.ps1 -Line 27
+.\extract.ps1 $request -TableNumber $TableNumber | Format-Table -Auto | Out-File -FilePath $KenpomPath
 <#
     espn bracket page
 #>
-$WebPage2 = $ConfigFile.Settings.scrape.espn.WebPage
-$WebPage2
-$content2 = $wc.DownloadString($WebPage2)
-$FilePath2 = $WorkDirectory + "\espn.html"
-$content2 | Out-File -FilePath $FilePath2
-exit
-<#
-    Networked drive mappings
-    Here is where the H:\, P:\ and T:\ drives are tested and set from the script (if they are not yet set up)
-#>
-write-host "Current user: $CurrentUser" -foreground "yellow"
-write-host "Current domain: $([Environment]::UserDomainName)" -foreground "yellow"
-write-host "Current machine: $([Environment]::MachineName)" -foreground "yellow"
-
-if (-Not (Test-Path h:))
-{
-  $UserHDrive = $ConfigFile.Settings.Network.User.$($CurrentUser).HDrive
-  if ($UserHDrive -gt "")
-  {
-    & net use h: $UserHDrive /persist:yes
-  }
-  if (-Not (Test-Path h:))
-  {
-    $WarnSetup = "network H:\ Does not exist, You need to set this up first (New version?)"
-    write-host $WarnSetup -foreground "red"
-    Exit
-  }
-}
-
-if (-Not (Test-Path p:))
-{
-  $UserPDrive = $ConfigFile.Settings.Network.PDrive
-  if ($UserPDrive -gt "")
-  {
-    & net use p: $UserPDrive /persist:yes
-  }
-  if (-Not (Test-Path p:))
-  {
-    $WarnSetup = "network P:\ Does not exist, You need to set this up first (New version?)"
-    write-host $WarnSetup -foreground "red"
-    Exit
-  }
-}
-
-if (-Not (Test-Path t:))
-{
-  $UserTDrive = $ConfigFile.Settings.Network.TDrive
-  if ($UserTDrive -gt "")
-  {
-    & net use t: $UserTDrive /persist:yes
-  }
-  if (-Not (Test-Path t:))
-  {
-    $WarnSetup = "network T:\ Does not exist, You need to set this up first (New version?)"
-    write-host $WarnSetup -foreground "red"
-    Exit
-  }
-}
-<#
-    This code parses out the Major and Minor version numbers
-#>
-$CoreVersion = $ConfigFile.Settings.CoreVersion
-$Pieces = $CoreVersion.split(".")
-if ($Pieces[1].Length -eq "")
-{
-  $Pieces = $CoreVersion.split("_")
-}
-$MajorVersion = $Pieces[0]
-$MinorVersion = $Pieces[1]
-
-write-host "Core version: $($MajorVersion).$($MinorVersion)" -foreground "magenta"
-
-<#
-    Global config settings from the Settings section
-#>
-$ASNCorePath = $PDriveRoot + $ConfigFile.Settings.ASNCoreFolder + "\" + $MajorVersion + "_" + $MinorVersion + "\"
-if (-Not (Test-Path $ASNCorePath))
-{
-  $WarnSetup = $ASNCorePath + " Does not exist, You need to set this up first (New version?)"
-  write-host $WarnSetup -foreground "red"
-  Exit
-}
-$TempFileLocation = $ASNCorePath + $ConfigFile.Settings.TempFileFolder + "\"
-if (-Not (Test-Path $TempFileLocation))
-{
-  $WarnSetup = $TempFileLocation + " Does not exist, You need to set this up first (New version?)"
-  write-host $WarnSetup -foreground "red"
-  Exit
-}
-
-<#
-    This script's config settings from the LoadUCData section
-#>
-$Tool = $PDriveRoot + $ConfigFile.Settings.ToolFolder + "\" + $ConfigFile.Settings.LoadUCData.ToolName
-if (-Not (Test-Path $Tool))
-{
-  $WarnSetup = $Tool + " Does not exist, You need to set this up first (New version?)"
-  write-host $WarnSetup -foreground "red"
-  Exit
-}
-
-$LoadUCDataFile = $PDriveRoot + $ConfigFile.Settings.LoadUCData.LoadUCDataFolder
-$LoadUCDataOld = $TempFileLocation + "LoadUCData.old"
-$LoadUCDataASN = $ASNCorePath + $ConfigFile.Settings.LoadUCData.ASNFileName
-$LoadUCDataTempFile = $TempFileLocation + "LoadUCData.sql"
-
-cd $ASNCorePath
-Convert-Path .
-If (Test-Path $LoadUCDataOld)
-{
-	write-host "$(get-date) Removing old LoadUCData file" -foreground "green"
-	Remove-Item $LoadUCDataOld -force
-}
-If (Test-Path $LoadUCDataFile)
-{
-	write-host "$(get-date) Copying LoadUCData.sql to LoadUCData.old" -foreground "green"
-  Copy-Item -Path $LoadUCDataFile -Destination $LoadUCDataOld -Force
-}
-$itemtime = Get-Date
-write-host "$(get-date) Generating LoadUCData.sql (oracle DB only)" -foreground "green"
-$outputTool = & $Tool ora, $LoadUCDataASN, $LoadUCDataTempFile
-$outputTool
-if ($outputTool -match "error")
-{
-  $elapsed = GetElapsedTime $itemtime
-  write-host "Elapsed Time: " $elapsed -foreground "red"
-}
-else
-{
-  $elapsed = GetElapsedTime $itemtime
-  write-host "Elapsed Time: " $elapsed -foreground "green"
-  If (Test-Path $LoadUCDataFile)
-  {
-    write-host "$(get-date) Removing production LoadUCData file" -foreground "green"
-    Remove-Item $LoadUCDataFile -force
-  }
-  write-host "$(get-date) Copying LoadUCData.sql to production" -foreground "green"
-  Copy-Item $LoadUCDataTempFile $LoadUCDataFile
-}
+$WebPage = $ConfigFile.Settings.scrape.espn.WebPage
+$WebPage
+$content = $wc.DownloadString($WebPage)
+$BracketPath = $WorkDirectory + "\espn.html"
+$content | Out-File -FilePath $BracketPath
 
 cd $PSScriptRoot
 Convert-Path .
